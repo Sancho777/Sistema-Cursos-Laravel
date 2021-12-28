882b208481517579b430c7595edf5c29a8e10668
Zebu777 <adrianomsancho@gmail.com>
2021-12-28 00:34:12 -0300

    Local Files Admin painel

---
+++ resources/views/login/adicionar.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.site')
+
+@section('titulo','Cursos')
+
+@section('conteudo')
+<div class="container">
+  <h3 class="center">Entrar</h3>
+  <div class="row">
+    <form class="" action="{{route('site.login.entrar')}}" method="post" enctype="multipart/form-data">
+      {{ csrf_field() }}
+
+      <div class="input-field">
+        <label>E-mail</label>
+        <input type="text" name="email">
+      </div>
+      <div class="input-field">
+        <label>Senha</label>
+        <input type="password" name="senha">
+      </div>
+      <button class="btn deep-orange">Entrar</button>
+
+    </form>
+  </div>
+</div>
+
+@endsection
